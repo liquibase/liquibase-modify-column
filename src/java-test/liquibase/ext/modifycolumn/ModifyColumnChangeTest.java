7e731bb5b654d1aae64b77c63f6be4f1ba736d2c
nvoxland <nvoxland@e6edf6fb-f266-4316-afb4-e53d95876a76>
2009-10-04 17:36:40 +0000

    moved modifyColumn from core to contrib
    git-svn-id: http://liquibase.jira.com/svn/CONTRIB/trunk/modify-column/trunk@1200 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ src/java-test/liquibase/ext/modifycolumn/ModifyColumnChangeTest.java
@@ -0,0 +1,48 @@
+package liquibase.ext.modifycolumn;
+
+
+import liquibase.change.ColumnConfig;
+import liquibase.change.core.*;
+import liquibase.change.core.ModifyColumnChange;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests for {@link liquibase.change.core.ModifyColumnChange}
+ */
+public abstract class ModifyColumnChangeTest { //extends AbstractChangeTest {
+
+//    liquibase.change.core.ModifyColumnChange change;
+//
+//    @Before
+//    public void setUp() throws Exception {
+//        change = new ModifyColumnChange();
+//        change.setTableName("TABLE_NAME");
+//
+//        ColumnConfig col1 = new ColumnConfig();
+//        col1.setName("NAME");
+//        col1.setType("integer(3)");
+//
+//        change.addColumn(col1);
+//    }
+//
+//    @Override
+//    @Test
+//    public void getRefactoringName() throws Exception {
+//        assertEquals("Modify Column", change.getChangeMetaData().getDescription());
+//    }
+//
+////    @Test
+////    public void generateStatement() throws Exception {
+////        OracleDatabase database = new OracleDatabase();
+////        assertEquals("ALTER TABLE TABLE_NAME MODIFY (NAME integer(3))", change.generateStatements(database)[0].getSqlStatement(database));
+////    }
+//
+//    @Override
+//    @Test
+//    public void getConfirmationMessage() throws Exception {
+//        assertEquals("Columns NAME(integer(3)) of TABLE_NAME modified", change.getConfirmationMessage());
+//    }
+//
+}
