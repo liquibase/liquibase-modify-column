85f546bdff9ea01c244521a201cacf130d7d82e5
Nathan Voxland <nathan@voxland.net>
2013-10-14 09:02:49 -0500

    Reenabled test

--- src/test/java/liquibase/ext/modifycolumn/ModifyColumnChangeTest.java
+++ src/test/java/liquibase/ext/modifycolumn/ModifyColumnChangeTest.java
@@ -1,39 +1,47 @@
 package liquibase.ext.modifycolumn;
 
 
+import liquibase.change.ChangeFactory;
 import liquibase.change.ColumnConfig;
 import liquibase.change.core.*;
 import static org.junit.Assert.*;
+
+import liquibase.database.core.OracleDatabase;
+import liquibase.sqlgenerator.SqlGeneratorFactory;
 import org.junit.Before;
 import org.junit.Test;
 
-public abstract class ModifyColumnChangeTest { //extends AbstractChangeTest {
+public class ModifyColumnChangeTest { //extends AbstractChangeTest {
 
-//    liquibase.change.core.ModifyColumnChange change;
-//
-//    @Before
-//    public void setUp() throws Exception {
-//        change = new ModifyColumnChange();
-//        change.setTableName("TABLE_NAME");
-//
-//        ColumnConfig col1 = new ColumnConfig();
-//        col1.setName("NAME");
-//        col1.setType("integer(3)");
-//
-//        change.addColumn(col1);
-//    }
-//
-//    @Override
-//    @Test
-//    public void getRefactoringName() throws Exception {
-//        assertEquals("Modify Column", change.getChangeMetaData().getDescription());
-//    }
-//
-////    @Test
-////    public void generateStatement() throws Exception {
-////        OracleDatabase database = new OracleDatabase();
-////        assertEquals("ALTER TABLE TABLE_NAME MODIFY (NAME integer(3))", change.generateStatements(database)[0].getSqlStatement(database));
-////    }
+    ModifyColumnChange change;
+
+    @Before
+    public void setUp() throws Exception {
+        change = new ModifyColumnChange();
+        change.setTableName("TABLE_NAME");
+
+        ColumnConfig col1 = new ColumnConfig();
+        col1.setName("NAME");
+        col1.setType("integer(3)");
+
+        change.addColumn(col1);
+    }
+
+    @Test
+    public void getRefactoringName() throws Exception {
+        assertEquals("modifyColumn", ChangeFactory.getInstance().getChangeMetaData(change).getName());
+    }
+
+    @Test
+    public void getRefactoringDescription() throws Exception {
+        assertEquals("Modify column definition", ChangeFactory.getInstance().getChangeMetaData(change).getDescription());
+    }
+
+    @Test
+    public void generateStatement() throws Exception {
+        OracleDatabase database = new OracleDatabase();
+        assertEquals("ALTER TABLE TABLE_NAME MODIFY ( NAME INTEGER )", SqlGeneratorFactory.getInstance().generateSql(change.generateStatements(database)[0], database)[0].toSql());
+    }
 //
 //    @Override
 //    @Test
